b5a81d03ebd044129e9d09568b6f5a8f746e03a2
Dipesh Paudel <thedeepyash7@gmail.com>
2024-04-26 19:11:49 +0545

    add production setup

---
+++ 13-production-setup/my_project/posts/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Post
+from . import forms
+
+# Create your views here.
+
+# Posts Views
+def post_list(request):
+  posts = Post.objects.all().order_by("-date")
+  return render(request, 'posts/posts_list.html', {
+    'posts': posts
+  })
+
+# Post View
+def post_page(request, slug):
+  post = Post.objects.get(slug=slug)
+  return render(request, 'posts/post_page.html', {
+    'post': post
+  })
+
+@login_required(login_url="/users/login/")
+
+# New Post View
+def post_new(request):
+  if request.method == "POST":
+    form = forms.CreatePost(request.POST, request.FILES)
+    if form.is_valid():
+      new_post = form.save(commit=False)
+      new_post.author = request.user
+      new_post.save()
+      return redirect('posts:list')
+  else:
+    form = forms.CreatePost()
+  return render(request, 'posts/post_new.html', { 'form': form })
